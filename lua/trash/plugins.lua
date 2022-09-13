6479ab68dc699bb640cf25ad3371c60628ef7aa7
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2022-09-14 00:50:42 +0100

    feat: adds indent_blankline

--- lua/trash/plugins.lua
+++ lua/trash/plugins.lua
@@ -86,6 +86,7 @@ return packer.startup(function(use)
 	use("lewis6991/impatient.nvim")
   use("goolord/alpha-nvim")
   use("phaazon/hop.nvim")
+  use("lukas-reineke/indent-blankline.nvim")
 
 	if PACKER_BOOTSTRAP then
 		require("packer").sync()
