6f679f98f4c7c669c163700a0bbeb95a089e2196
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2022-09-13 19:10:44 +0100

    feat: adds cmp

--- lua/trash/plugins.lua
+++ lua/trash/plugins.lua
@@ -59,6 +59,16 @@ return packer.startup(function(use)
   -- Telescope
   use("nvim-telescope/telescope.nvim")
 
+  -- CMP
+  use("hrsh7th/nvim-cmp")
+	use("hrsh7th/cmp-buffer")
+	use("hrsh7th/cmp-path")
+	use("hrsh7th/cmp-cmdline")
+	use("saadparwaiz1/cmp_luasnip")
+	use("hrsh7th/cmp-nvim-lsp")
+	use("L3MON4D3/LuaSnip")
+	use("rafamadriz/friendly-snippets")
+
 	if PACKER_BOOTSTRAP then
 		require("packer").sync()
 	end
