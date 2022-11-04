261e7b68017e0bb8ea50b52716fbd5e683fc9b8c
Ofek Asido <asido.ofek@gmail.com>
2022-11-04 20:22:29 +0200

    fix: bug causing errors on launch
    I caused this one, sorry!
    Didn't include parentheses on my last pull request… I broke your configuration files!
    I got an error message every time I opened files, and LSP didn't work.
    I asked for help in Primeagen's Discord, and a lovely person named Nobody#4931 found this bug within two seconds.
    
    Sorry!

--- lua/trash/lsp/init.lua
+++ lua/trash/lsp/init.lua
@@ -48,7 +48,7 @@ if not cmp_nvim_lsp then
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = cmp_nvim_lsp.default_capabilities
+capabilities = cmp_nvim_lsp.default_capabilities()
 
 local opts = {
 	capabilities = capabilities,
