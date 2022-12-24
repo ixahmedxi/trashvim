1fc762d2def4b0c251acaceb199162a8f8a27916
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2022-12-24 15:47:31 +0000

    feat: new colorscheme & cmp improvements

--- lua/config/colorschemes.lua
+++ lua/config/colorschemes.lua
@@ -1,4 +1,40 @@
-vim.g.mellow_transparent = true
-vim.g.material_style = "deep ocean"
-
-vim.cmd([[colorscheme catppuccin]])
+import("catppuccin", function(catppuccin)
+  catppuccin.setup({
+    flavour = "mocha", -- latte, frappe, macchiato, mocha
+    term_colors = true,
+    transparent_background = false,
+    no_italic = false,
+    no_bold = false,
+    styles = {
+      comments = {},
+      conditionals = {},
+      loops = {},
+      functions = {},
+      keywords = {},
+      strings = {},
+      variables = {},
+      numbers = {},
+      booleans = {},
+      properties = {},
+      types = {},
+    },
+    color_overrides = {
+      mocha = {
+        base = "#000000",
+      },
+    },
+    highlight_overrides = {
+      mocha = function(C)
+        return {
+          TabLineSel = { bg = C.pink },
+          NvimTreeNormal = { bg = C.none },
+          CmpBorder = { fg = C.surface2 },
+          Pmenu = { bg = C.none },
+          NormalFloat = { bg = C.none },
+          TelescopeBorder = { link = "FloatBorder" },
+        }
+      end,
+    },
+  })
+  vim.cmd.colorscheme("catppuccin")
+end)
